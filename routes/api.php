ae87f0dd79c00b5019c0ab25b1b9283f45b4d941
Agung Hasbi <agunghasbi13@gmail.com>
2023-07-09 15:15:01 +0700

    Payment Methods

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DataPlanController;
 use App\Http\Controllers\Api\OperatorCardController;
+use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\TopUpController;
 use App\Http\Controllers\Api\TransferController;
 use App\Http\Controllers\Api\WebhookController;
@@ -38,4 +39,5 @@ Route::group(['middleware' => 'jwt.verify'], function ($router) {
     Route::post('transfers', [TransferController::class, 'store']);
     Route::post('data_plans', [DataPlanController::class, 'store']);
     Route::get('operator_cards', [OperatorCardController::class, 'index']);
+    Route::get('payment_methods', [PaymentMethodController::class, 'index']);
 });
