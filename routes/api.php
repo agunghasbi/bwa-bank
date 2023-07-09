a1abedf127359436395e4e83887d9d520731e77f
Agung Hasbi <agunghasbi13@gmail.com>
2023-07-09 15:27:38 +0700

    Transfer Histories

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\OperatorCardController;
 use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\TopUpController;
 use App\Http\Controllers\Api\TransferController;
+use App\Http\Controllers\Api\TransferHistoryController;
 use App\Http\Controllers\Api\WebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -40,4 +41,5 @@ Route::group(['middleware' => 'jwt.verify'], function ($router) {
     Route::post('data_plans', [DataPlanController::class, 'store']);
     Route::get('operator_cards', [OperatorCardController::class, 'index']);
     Route::get('payment_methods', [PaymentMethodController::class, 'index']);
+    Route::get('transfer_histories', [TransferHistoryController::class, 'index']);
 });
