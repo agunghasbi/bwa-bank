041f838f67d85d62c57bfd3822bd4d4c2ae6def3
Agung Hasbi <agunghasbi13@gmail.com>
2023-07-13 20:35:22 +0700

    add uploadBase64Image()

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,9 @@
 
 use App\Models\User;
 use App\Models\Wallet;
+use Illuminate\Support\Facades\Storage;
+use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
+use Illuminate\Support\Str;
 
 function getUser($param)
 {
@@ -29,4 +32,16 @@ function pinChecker($pin)
 
     if ($wallet->pin == $pin) return true;
     return false;
+}
+
+function uploadBase64Image($base64Image)
+{
+    $decoder = new Base64ImageDecoder($base64Image, $allowedFormats = ['jpeg', 'png', 'jpg']);
+
+    $decodedContent = $decoder->getDecodedContent();
+    $format = $decoder->getFormat();
+    $image = Str::random(10) . '.' . $format;
+    Storage::disk('public')->put($image, $decodedContent);
+
+    return $image;
 }
