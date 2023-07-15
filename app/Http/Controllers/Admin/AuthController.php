eab5614f7f25c1674abcfe21b5e0b455e61dc185
Agung Hasbi <agunghasbi13@gmail.com>
2023-07-15 16:30:08 +0700

    Admin Page

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credential = $request->only('email', 'password');
+
+        if (Auth::guard('web')->attempt($credential)) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect()->back()->with('error', 'Invalid Credential')->withInput();
+    }
+
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+
+        return redirect()->route('admin.auth.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController as AdminAuthController;
+use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\RedirectPaymentController;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +22,14 @@ Route::get('/', function () {
 });
 
 Route::get('/payment_finish', [RedirectPaymentController::class, 'finish']);
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::view('/login', 'login')->name('admin.auth.index');
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.auth.login');
+    Route::get('/logout', [AdminAuthController::class, 'logout'])->name('admin.auth.logout');
+
+    Route::group(['middleware' => 'auth:web'], function () {
+        Route::view('/', 'dashboard')->name('admin.dashboard');
+        Route::get('/transaction', [TransactionController::class, 'index'])->name('admin.transaction.index');
+    });
+});
